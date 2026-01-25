61d89d61e7fe6206c643da6368a5d656dca33868
aesparzaunam <64211319+aesparzaunam@users.noreply.github.com>
2026-01-25 01:02:52 -0600

    24012026 - test3

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,12 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-function createPrismaClient() {
-    const adapter = new PrismaBetterSqlite3({
-        url: process.env.DATABASE_URL ?? "file:./prisma/dev.db",
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        // opcional: logs en dev
+        // log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
     });
-    return new PrismaClient({ adapter });
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
